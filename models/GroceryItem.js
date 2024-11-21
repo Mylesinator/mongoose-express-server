4a301d499c430948d849d74b4ffd3e1d867cd3b1
Myles Marshall <144744420+Mylesinator@users.noreply.github.com>
2024-11-21 00:06:46 +0000

    11/20/24

--- models/GroceryItem.js
+++ models/GroceryItem.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const grocerySchema = new mongoose.Schema({
     item: {
         type: String,
-        required: [true, "Item name is required"]
+        required: [true, 'item name is required']
     },
     food_group: {
         type: String,
-        required: [true, "food_group is required"],
-        enum: ["fruits", "dairy", "proteins", "vegetables", "nuts", "grains"]
+        required: [true, 'food_group is required'],
+        enum: ['fruits', 'dairy', 'proteins', 'vegetables', 'nuts', 'grains']
     },
     price_in_usd: {
         type: Number,
-        required: [true, "price_in_usd is required"],
-        min: [0, "Price must be a positive number"]
+        required: [true, 'Please enter a number for price_in_usd'],
+        min: [0, 'Price is required and must be a positive number']
     }
 });
 
-module.exports = mongoose.model("GroceryItem", grocerySchema);
+module.exports = mongoose.model('GroceryItem', grocerySchema);
